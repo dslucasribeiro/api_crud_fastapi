70180b46d3a529199d0810343f93460901042e48
dslucasribeiro <dslucasribeiro@gmail.com>
2024-02-20 08:46:45 -0300

    First commit

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_main_status_code():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_main_response():
+    response = client.get("/")
+    assert response.json() == {"Hello": "World"}
+
+def test_listar_produtos():
+    response = client.get("/produtos")
+    assert response.status_code == 200
+    
+def test_tamanho_lista():
+    response = client.get("/produtos")
+    assert len(response.json()) == 3
